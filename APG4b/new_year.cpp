205b33ec46bb366eaf04dc8e9fa18fddc0fd4b00
Ryota Kato <rka147@sfu.ca>
2025-03-22 22:37:50 -0700

    add A questions

---
+++ APG4b/new_year.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int day = 24;
+  int M;
+  
+  cin >> M;
+  
+  int remain;
+  remain = day + (day - M);
+  
+  cout << remain << endl;
+  
+  return 0;
+  
+} 
---
+++ APG4b/rating_goal.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+  int R, G, ans;
+
+  cin >> R >> G;
+
+  ans = G * 2 - R;
+
+  cout << ans << endl;
+
+
+
+  return 0;
+}
+
